8a552eb7b4c60e6c9958068b4dc35d9889c632b5
Janbluus <qilei0529@gmail.com>
2024-05-21 10:48:32 +0800

    base flutter create files

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_game_2048_fight
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_game_2048_fight/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_game_2048_fight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
